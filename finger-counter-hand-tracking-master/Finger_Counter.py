bc439cd1988aae572b3d67419efe8ce961ae30f9
pascal-maker <pascalmusabyimana681@gmail.com>
2023-10-06 23:58:18 +0200

    fixed

--- finger-counter-hand-tracking-master/Finger_Counter.py
+++ finger-counter-hand-tracking-master/Finger_Counter.py
@@ -8,11 +8,11 @@ cam_width, cam_height = 640, 480
 previous_time = 0
 #############################################################
 
-cap = cv2.VideoCapture(0)
-cap.set(3, cam_width)
-cap.set(4, cam_height)
-
+# Create the 'finger_image' directory if it doesn't exist
 folder_path = "finger_image"
+if not os.path.exists(folder_path):
+    os.mkdir(folder_path)
+
 my_list = os.listdir(folder_path)
 overlay_list = []
 
@@ -20,12 +20,15 @@ for path in my_list:
     image = cv2.imread(f'{folder_path}/{path}')
     overlay_list.append(image)
 
-
 detector = ht.HandDetector(detection_confidence=0.70)
 
 tip_id = [4, 8, 12, 16, 20]
 
-while cap.isOpened() :
+cap = cv2.VideoCapture(0)
+cap.set(3, cam_width)
+cap.set(4, cam_height)
+
+while cap.isOpened():
     success, img = cap.read()
     img = detector.find_hands(img)
 
@@ -36,8 +39,13 @@ while cap.isOpened() :
 
         total_fingers = fingers.count(1)
 
-        h, w, c = overlay_list[total_fingers].shape
-        img[0:h, 0:w] = overlay_list[total_fingers]
+        if total_fingers < len(overlay_list):
+            h, w, c = overlay_list[total_fingers].shape
+            img[0:h, 0:w] = overlay_list[total_fingers]
+        else:
+            # Handle the case when total_fingers is out of range
+            # You can display a message or take appropriate action
+            pass  # Replace 'pass' with your desired error handling or action
 
         cv2.rectangle(img, (20, 300), (170, 475), (0, 255, 0), cv2.FILLED)
         cv2.putText(img, text=str(total_fingers), org=(50, 450), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=8,
